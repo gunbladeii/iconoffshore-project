7cf549a2e9cbd122ba6188f209082de7dfc3fe20
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-07-05 23:43:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -34,6 +34,7 @@
 			<div class="wrap-login100 p-t-30 p-b-50">
 				<span class="login100-form-title p-b-41">
 					<img src="assets/img/apple-icon.png" class="img-thumbnail img-fluid">
+					Account Login
 				</span>
 				<form class="login100-form validate-form p-b-33 p-t-5">
 
