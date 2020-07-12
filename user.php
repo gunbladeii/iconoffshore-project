b39f6d903bcb33adb69792ff95c964291bb55174
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-07-12 09:59:56 +0800

    Update user.php

--- user.php
+++ user.php
@@ -161,7 +161,7 @@
       <div class="content">
         <div class="container-fluid">
           <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-12">
               <div class="card">
                 <div class="card-header card-header-primary">
                   <h4 class="card-title">User Registration</h4>
